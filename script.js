10a955281f689929d3bceff86164c40532545543
dartfalak <falakcles@gmail.com>
2025-09-18 20:27:02 +0500

    implement basic game logic for tic tac toe

---
+++ script.js
@@ -0,0 +1,14 @@
+let blocks = document.querySelectorAll('.block');
+let resetButton = document.getElementById('#reset');
+
+let currentPlayer0 = true;
+
+const board = [ 
+    [0,1,2],
+
+
+
+
+
+
+]
