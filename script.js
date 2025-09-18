59fc64b4ac5cc094a63dc2eea407b9f680199193
dartfalak <falakcles@gmail.com>
2025-09-18 20:48:23 +0500

    implement basic game logic for tic tac toe

--- script.js
+++ script.js
@@ -34,9 +34,21 @@ blocks.forEach((block) => {
 });
 
 const checkWin = () => {
-    for(pattern of board) {
-        console.log(pattern);
+    for( let pattern of board) {
+        console.log
+        (blocks[pattern[0]].innerText,
+        blocks[pattern[1]].innerText,
+        blocks[pattern[2]].innerText
+        );
 
+        let pos1Value = blocks[pattern[0]].innerText;
+        let pos2Value = blocks[pattern[1]].innerText;
+        let pos3Value = blocks[pattern[2]].innerText;
+
+        if(pos1Value !== '' && pos1Value === pos2Value && pos2Value === pos3Value) {
+            if(pos1Value === pos2Value && pos2Value === pos3Value) {
+                alert(`${pos1Value} has won!`);
+        }
     } 
 
 };
