54c119008e3631015c626c4178266fc9f196124e
dartfalak <falakcles@gmail.com>
2025-09-18 20:30:26 +0500

    implement basic game logic for tic tac toe

--- script.js
+++ script.js
@@ -5,10 +5,20 @@ let currentPlayer0 = true;
 
 const board = [ 
     [0,1,2],
-
-
-
-
+    [0,3,6],
+    [0,4,8],
+    [1,4,7],
+    [2,5,8],
+    [2,4,6],
+    [3,4,5],
+    [6,7,8],
 
 
 ]
+
+blocks.forEach((block) => {
+    block.addEventListener('click', () => {
+        console.log('clicked');
+
+    })
+});
